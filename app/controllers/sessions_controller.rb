218bb87cbae92687ddcca88c9c7c0d6adb6612ed
yomaytk <ym.atonement.oy999@gmail.com>
2021-09-30 17:33:29 +0900

    halfway of session.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
+  
   def new
   end
+
+  def create
+    user = User.find_by(name: params[:session][:name])
+    if user && user.authenticate(params[:session][:password])
+      login user
+      redirect_to '/'
+    end
+    # TODO faied to login
+  end
+
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,16 @@
 module SessionsHelper
+
+    def login(login_user)
+        session[:user_id] = login_user.id
+    end
+
+    def current_user
+        if !session[:user_id].nil?
+            @current_user ||= User.find_by(id: session[:user_id])
+        end
+    end
+
+    def logged_in?
+        !current_user.nil?
+    end
 end
