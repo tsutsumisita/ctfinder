1fb76123081853fad6793ff0992df3e4d21609f2
yomaytk <ym.atonement.oy999@gmail.com>
2021-09-30 22:04:12 +0900

    fix tournament show.

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -2,9 +2,10 @@ class TournamentsController < ApplicationController
   
   def show
     @tournament = Tournament.find(params[:id])
-    redirect_to root_url unless @tournament.nil?
-    # @participants = Participants.find_by(tournament_id: @tournament.id)
-    # @bss = Bss.find_by(bss_id: @bss.id)
+    redirect_to root_url if @tournament.nil?
+    @participants = @tournament.participants
+    # @participants = Participant.find_by(tournament_id: @tournament.id)
+    # @bss = Bss.find_by(tournament_id: @tournament.id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
   
-  get 'tournaments/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   # root path
   root 'static_pages#home'
   
   resources :users, only: [:show, :create]
+  resources :tournaments, only: [:show]
   
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
-  
-  
+
 end
