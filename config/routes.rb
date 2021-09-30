4020e568fbe991e4480c40160192433708365862
yomaytk <ym.atonement.oy999@gmail.com>
2021-09-30 16:52:57 +0900

    Add user controller routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'static_pages/home'
-  get 'users/new'
-  get 'users/edit'
-  get 'users/show'
-  get 'users/create'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+  resources :users, only: [:new, :create, :show]
+  
 end
