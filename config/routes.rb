e88df85436e2de81e45101803dc368c2c10a1dcc
yomaytk <ym.atonement.oy999@gmail.com>
2021-09-30 15:26:02 +0900

    Add User controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/edit'
+  get 'users/show'
+  get 'users/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
