e9bc1a4c3162a9a06c084df27abc46dbf0141e71
sequoia-choco <sequoia.chocolate@gmail.com>
2021-10-04 01:03:31 +0900

    edit direct_messages_controller.rb

--- app/controllers/direct_messages_controller.rb
+++ app/controllers/direct_messages_controller.rb
@@ -1,2 +1,17 @@
 class DirectMessagesController < ApplicationController
+
+  def create
+    matching = Matching.find_by(user1_id: session[:user_id], user2_id: params[:id])
+    if matching.nil? then matching = Matching.find_by(user1_id: params[:id], user2_id: session[:user_id]) end
+
+    unless matching.nil? then
+      @matching = matching
+      @content = paramas[:content]
+        
+    else
+      flash.now[:danger] = "ダイレクトメッセージの送信に失敗しました"
+      render 'show'
+    end
+
+  end
 end
