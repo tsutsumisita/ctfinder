b9f71af8a6fa472d4537eabf231e09c2ddd64355
sequoia-choco <sequoia.chocolate@gmail.com>
2021-10-04 21:55:39 +0900

    users_helper.rb

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,7 @@ module UsersHelper
 
   def matching_find
     @matching = Matching.find_by(user1_id: session[:user_id], user2_id: params[:id])
-    @matching = Matching.find_by(user1_id: params[:id], user2_id: session[:user_id]) if matching.nil?
+    @matching = Matching.find_by(user1_id: params[:id], user2_id: session[:user_id]) if @matching.nil?
   end
 
 end
