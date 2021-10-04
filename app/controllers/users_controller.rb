b9ba9072b57fab8cf863770b87eddc124016b429
touri <20143537ok@gmail.com>
2021-10-04 13:54:39 +0900

    login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,6 +32,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
+      login user
       redirect_to root_url, success: "CtFinderへようこそ！"
     else
       flash.now[:danger] = "ユーザーの保存に失敗しました"
