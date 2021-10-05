562a3e5ff5fadf56535fd29e53ae328ae01f5ceb
Koji Nagatomi <nagatomi.koji.ne0@is.naist.jp>
2021-10-05 15:13:21 +0900

    flash messages

--- app/controllers/direct_messages_controller.rb
+++ app/controllers/direct_messages_controller.rb
@@ -12,10 +12,11 @@ class DirectMessagesController < ApplicationController
       dm.matching = @matching
       dm.sender_id = current_user.id
       if dm.save
-        p dm
-	      redirect_to "/users/#{params[:id]}/direct_messages/", notice: "ダイレクトメッセージが送信されました"
+        flash[:success] = "ダイレクトメッセージが送信されました"
+        redirect_to "/users/#{params[:id]}/direct_messages/"
       else
-        flash.now[:danger] = "ダイレクトメッセージの送信に失敗しました"
+        flash[:danger] = "ダイレクトメッセージの送信に失敗しました"
+        redirect_to "/users/#{params[:id]}/direct_messages/"
       end
     end
   end
