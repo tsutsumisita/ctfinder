0f3c85c652ff605c54eb65a925ec6e50beb92b5e
yomaytk <ym.atonement.oy999@gmail.com>
2021-10-05 22:32:11 +0900

    Add Like validation.

--- app/models/like.rb
+++ app/models/like.rb
@@ -12,5 +12,6 @@ class Like < ApplicationRecord
     belongs_to :liker, class_name: "User"
     belongs_to :liked, class_name: "User"
 
+    validates :liker_id, uniqueness: { scope: [:liked_id] }
     validates_with LikeIdValidator
 end
